d2ca01868bcdfde60706a965fa9771e98ef95920
samruddhi-tonghale <samruddhitonghale17@gmail.com>
2025-11-02 20:01:16 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from ultralytics import YOLO
+import cv2
+import numpy as np
+import pandas as pd
+from PIL import Image
+import os
+import sqlite3
+import datetime
+import pytesseract
+import plotly.express as px
+import tempfile
